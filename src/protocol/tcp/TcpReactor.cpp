f60aabfef7a3641bc12ac713398a7be4a69fb332
tty0929 <jhkim000929@gmail.com>
2026-02-09 09:59:31 +0900

    2026-02-09

--- src/protocol/tcp/TcpReactor.cpp
+++ src/protocol/tcp/TcpReactor.cpp
@@ -14,8 +14,8 @@
 
 #define TCP_RECV_CHUNK_SIZE     (4096)
 #define TCP_MAX_EVENTS          (64)
-#define TCP_MAX_RX_BUFFER_SIZE  (256 * 1024);
-#define TCP_MAX_TX_BUFFER_SIZE  (256 * 1024);
+#define TCP_MAX_RX_BUFFER_SIZE  (256 * 1024)
+#define TCP_MAX_TX_BUFFER_SIZE  (256 * 1024)
 
 TcpReactor::TcpReactor(int port, TcpWorker* tcpWorker, std::shared_ptr<TlsServer> tlsServer) :
     m_port(port),
